05ed425f18cb8067adb6cafe356b8e98745e0340
Tatia Gujabidze <121593341+tatiagujabidze@users.noreply.github.com>
2023-06-11 22:12:26 -0400

    Add files via upload

---
+++ JavaScript Homework 10/my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My name is Tatia and I'm a student at SkillWill Online Academy.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.skillwill.ge/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
